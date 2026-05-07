d4557ab5cab882b7bd95dc28c1fab65800f57ac7
FraterAstrust <FraterAstrust@proton.me>
2026-05-07 14:36:04 -0700

    update

--- netlify/functions/auth-patreon-callback.js
+++ netlify/functions/auth-patreon-callback.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 // Add your Patreon user ID here to always get adept access
-const ADMIN_IDS = ["57190794"];
+const ADMIN_IDS = ["YOUR_PATREON_USER_ID"];
 
 function getTier(amountCents, userId) {
   if (ADMIN_IDS.includes(userId)) return "adept";
